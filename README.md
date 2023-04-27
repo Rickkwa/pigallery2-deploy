62b2f190f578706e9b01de7ddc9ed1e323c1c848
Ricky Kwan <rickyk.162@gmail.com>
2023-04-26 21:43:29 -0400

    Initial deployment configs (#1)

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # pigallery2-deploy
 Deployment configs for https://github.com/bpatrik/pigallery2
+
+
+## Running it
+
+Workspace setup:
+
+```shell
+WORKDIR=/app
+git clone git@github.com:Rickkwa/pigallery2-deploy.git "${WORKDIR}"
+mkdir -p "${WORKDIR}/nginx/cache" "${WORKDIR}/pigallery2/images" "${WORKDIR}/pigallery2/tmp" "${WORKDIR}/pigallery2/config"
+```
+
+Then follow these steps:
+
+1. Docker compose up to do first time initialization.
+1. Create a new admin user and delete the `admin` user.
+1. Then stop the containers.
+1. Apply config.json according to the one in this repo.
+1. Start containers up again.
+1. Start uploading photos to `$WORKDIR/pigallery2/images/`
---
+++ custom-resources/custom.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Load custom theme
+    let themeTag = document.createElement("link");
+    themeTag.rel = "stylesheet";
+    themeTag.href = "/custom-resources/theme.css";
+    document.head.appendChild(themeTag);
+    
+    // Find the word "Photography" in the title and wrap it in <span> tag with white color
+    let titleEl = document.querySelector(".navbar-brand strong")
+    titleEl.innerHTML = titleEl.innerHTML.replace(/(photography)/ig, "<span style='color: white'>$1</span>")
+    
+    // Edit favicon
+    let faviconLink = document.querySelector("link[rel~='icon']");
+    if (!faviconLink) {
+        faviconLink = document.createElement('link');
+        faviconLink.rel = 'icon';
+        document.head.appendChild(faviconLink);
+    }
+    faviconLink.href = '/custom-resources/favicon/favicon.ico';
+    
+    // Analytics
+    let metricsTag = document.createElement("script")
+    metricsTag.src = "https://www.googletagmanager.com/gtag/js?id=G-KLLP7S6226";
+    metricsTag.async = true;
+    document.body.appendChild(metricsTag);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-KLLP7S6226');
+});
