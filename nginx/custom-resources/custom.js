c3a2e55b8e72bdcb607b06492f08c51ab5dec73c
Ricky Kwan <rickyk.162@gmail.com>
2023-05-22 21:01:50 -0400

    Keep and hide search (#16)

--- nginx/custom-resources/custom.js
+++ nginx/custom-resources/custom.js
@@ -4,11 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
     themeTag.rel = "stylesheet";
     themeTag.href = "/custom-resources/theme.css";
     document.head.appendChild(themeTag);
-    
+
     // Find the word "Photography" in the title and wrap it in <span> tag with white color
     let titleEl = document.querySelector(".navbar-brand strong")
     titleEl.innerHTML = titleEl.innerHTML.replace(/(photography)/ig, "<span style='color: white'>$1</span>")
-    
+
     // Edit favicon
     let faviconLink = document.querySelector("link[rel~='icon']");
     if (!faviconLink) {
@@ -17,7 +17,12 @@ document.addEventListener("DOMContentLoaded", function () {
         document.head.appendChild(faviconLink);
     }
     faviconLink.href = '/custom-resources/favicon/favicon.ico';
-    
+
+    // Edit icon for the Search button in navbar
+    let searchExpandBtn = document.querySelector("app-gallery-search > form > div > button:last-of-type");
+    searchExpandBtn.className = searchExpandBtn.className.replace("btn-light", "").replace("btn", "");
+    searchExpandBtn.querySelector("span").className = searchExpandBtn.querySelector("span").className.replace("oi-chevron-bottom", "oi-magnifying-glass");
+
     // Analytics
     let metricsTag = document.createElement("script")
     metricsTag.src = "https://www.googletagmanager.com/gtag/js?id=G-KLLP7S6226";
