e8fda1016f91c07e4b1a3b43c9410c2d3aab28fe
Ricky Kwan <rickyk.162@gmail.com>
2023-11-05 03:45:39 -0500

    Make padding part of map button clickable

--- nginx/custom-resources/custom.js
+++ nginx/custom-resources/custom.js
@@ -49,7 +49,7 @@ function activateCustomJS() {
             let overlayEl = mapBtn.querySelector("div.overlay");
             overlayEl.innerHTML = `<ng-icon name="ionFunnelOutline" title="Map" style="--ng-icon__size: 1em;"> <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512" style="width:var(--ng-icon__size, 1em);height:var(--ng-icon__size, 1em)"><path d="M313.27 124.64L198.73 51.36a32 32 0 00-29.28.35L56.51 127.49A16 16 0 0048 141.63v295.8a16 16 0 0023.49 14.14l97.82-63.79a32 32 0 0129.5-.24l111.86 73a32 32 0 0029.27-.11l115.43-75.94a16 16 0 008.63-14.2V74.57a16 16 0 00-23.49-14.14l-98 63.86a32 32 0 01-29.24.35zM328 128v336M184 48v336" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" style="stroke-width:var(--ng-icon__stroke-width, 32)"/></svg> </ng-icon>`;
             // Move to bar
-            mapEl.className = mapEl.className.replace("rounded", "btn btn-outline-secondary btn-navigator")
+            overlayEl.className = overlayEl.className + " btn btn-outline-secondary btn-navigator"
             let photoCountEl = document.querySelector("app-gallery-navbar div.photos-count");
             let divider = document.querySelector("app-gallery-navbar div.divider")
             photoCountEl.insertAdjacentElement("afterend", mapEl);
